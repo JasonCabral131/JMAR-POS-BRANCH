729916d6148a8ebc8897a158b3ba5c798cc50385
JasonCabral131 <jasoncabral329@gmail.com>
2021-12-21 21:46:56 +0800

    update

--- src/views/cashier/cashierSales/Sales/DailySales.jsx
+++ src/views/cashier/cashierSales/Sales/DailySales.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { CDataTable } from "@coreui/react";
+import { CDataTable, CCollapse, CCardBody } from "@coreui/react";
 import { AiOutlineDown, AiOutlineUp, AiOutlinePrinter } from "react-icons/ai";
 import { dailyFields } from "../salesWidget";
 const DailySaleInfo = ({ sales, loading }) => {
   const [details, setDetails] = useState([]);
+  const [dP, setDP] = useState([]);
   const toggleDetails = (index) => {
     const position = details.indexOf(index);
     let newDetails = details.slice();
@@ -14,6 +15,17 @@ const DailySaleInfo = ({ sales, loading }) => {
     }
     setDetails(newDetails);
   };
+  const toggleDownProduct = (index) => {
+    const position = dP.indexOf(index);
+    let newDetails = dP.slice();
+    if (position !== -1) {
+      newDetails.splice(position, 1);
+    } else {
+      newDetails = [...details, index];
+    }
+    setDP(newDetails);
+  };
+  const handlePrintAllDailySaleData = () => {};
   return (
     <div className="card shadow p-3" style={{ position: "relative" }}>
       <div className="card-header">
@@ -30,7 +42,17 @@ const DailySaleInfo = ({ sales, loading }) => {
             top: 20,
           }}
         >
-          <AiOutlinePrinter size="25" className="hover" />
+          {sales ? (
+            sales.salesByDay ? (
+              sales.salesByDay.length > 0 ? (
+                <AiOutlinePrinter
+                  size="25"
+                  className="hover"
+                  onClick={handlePrintAllDailySaleData}
+                />
+              ) : null
+            ) : null
+          ) : null}
         </div>
       </div>
       <div className="card-body">
@@ -47,6 +69,13 @@ const DailySaleInfo = ({ sales, loading }) => {
           pagination
           loading={loading}
           scopedSlots={{
+            totalAmount: (item) => (
+              <td className="fw-bolder">
+                {`₱ ${new Intl.NumberFormat().format(
+                  Math.round((item.totalAmount + Number.EPSILON) * 100) / 100
+                )}`}
+              </td>
+            ),
             show_details: (item, index) => (
               <td>
                 <div className="d-flex justify-content-center">
@@ -69,6 +98,115 @@ const DailySaleInfo = ({ sales, loading }) => {
                 </div>
               </td>
             ),
+            details: (item, index) => {
+              return (
+                <CCollapse show={details.includes(index)}>
+                  <CCardBody className={"p-2"}>
+                    <div className=" card shadow p-2">
+                      <h4 className="ml-2">{item.date + " List Of Data"}</h4>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "flex-end",
+                          alignItems: "flex-end",
+                          position: "absolute",
+                          right: 10,
+                          top: 20,
+                        }}
+                      >
+                        <AiOutlinePrinter size="25" className="hover" />
+                      </div>
+                      <div className="card-body ">
+                        <CDataTable
+                          items={[...item.data]}
+                          fields={fieldsDaily}
+                          columnFilter={false}
+                          tableFilter={{ placeholder: "search information.." }}
+                          footer={false}
+                          itemsPerPageSelect={true}
+                          itemsPerPage={5}
+                          hover
+                          sorter
+                          pagination
+                          scopedSlots={{
+                            total: (item) => (
+                              <td className="fw-bolder">
+                                {`₱ ${new Intl.NumberFormat().format(
+                                  Math.round(
+                                    (item.total + Number.EPSILON) * 100
+                                  ) / 100
+                                )}`}
+                              </td>
+                            ),
+                            show_details: (item, index) => (
+                              <td>
+                                <div className="d-flex justify-content-center">
+                                  <AiOutlinePrinter
+                                    size="20"
+                                    className="hover"
+                                  />
+                                  {dP.includes(index) ? (
+                                    <AiOutlineDown
+                                      onClick={() => {
+                                        toggleDownProduct(index);
+                                      }}
+                                      className="hover mt-1 ml-4"
+                                    />
+                                  ) : (
+                                    <AiOutlineUp
+                                      onClick={() => {
+                                        toggleDownProduct(index);
+                                      }}
+                                      className="hover mt-1 ml-4"
+                                    />
+                                  )}
+                                </div>
+                              </td>
+                            ),
+                            details: (item, index) => {
+                              return (
+                                <CCollapse show={dP.includes(index)}>
+                                  <CCardBody className={"p-2"}>
+                                    <div className=" card shadow p-2">
+                                      <h4 className="ml-2">
+                                        <span
+                                          style={{
+                                            color: "#adadad",
+                                            letterSpacing: 3,
+                                          }}
+                                        >
+                                          Product List
+                                        </span>
+                                      </h4>
+                                      <CDataTable
+                                        items={[
+                                          ...handleShowProduct(item.product),
+                                        ]}
+                                        fields={productFields}
+                                        columnFilter={false}
+                                        tableFilter={{
+                                          placeholder: "search product..",
+                                        }}
+                                        footer={false}
+                                        itemsPerPageSelect={true}
+                                        itemsPerPage={5}
+                                        hover
+                                        sorter
+                                        pagination
+                                      />
+                                    </div>
+                                  </CCardBody>
+                                </CCollapse>
+                              );
+                            },
+                          }}
+                        />
+                      </div>
+                    </div>
+                  </CCardBody>
+                </CCollapse>
+              );
+            },
           }}
         />
       </div>
@@ -76,3 +214,27 @@ const DailySaleInfo = ({ sales, loading }) => {
   );
 };
 export default DailySaleInfo;
+
+const fieldsDaily = [
+  { key: "salesId", label: "Transaction ID", _style: { width: "45%" } },
+  { key: "total", label: "Total Amount", _style: { width: "45%" } },
+
+  {
+    key: "show_details",
+    label: "",
+    _style: { width: "10%" },
+    sorter: false,
+    filter: false,
+  },
+];
+const productFields = [
+  { key: "product", label: "Product" },
+  { key: "price", label: "Price" },
+  { key: "quantity", label: "Quantity" },
+  { key: "amount", label: "Amount" },
+];
+const handleShowProduct = (item) => {
+  return item.map((data) => {
+    return { ...data, product: data.product.product };
+  });
+};
